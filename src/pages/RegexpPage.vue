4591b56c4a0f075a2a3878d736bd6d8c944e9925
UBiYaChi <2809750987@qq.com>
2023-08-13 13:55:17 +0800

    save

--- src/pages/RegexpPage.vue
+++ src/pages/RegexpPage.vue
@@ -28,7 +28,7 @@
         </div>
       </el-form-item>
     </el-form>
-    <el-collapse class="regexp-wrapper flex-1" :model-value="['common', 'character']">
+    <el-collapse class="regexp-wrapper flex-1 over-h" :model-value="['common']">
       <el-collapse-item
         v-for="(data, row) in regexpList"
         :key="row"
@@ -36,13 +36,13 @@
         :name="data.name"
       >
         <div
-          class="regexp-item flex-v"
+          class="regexp-item flex-v over-h"
           v-for="(item, col) in data.children"
           :key="col"
           @click="handleChoose(item.value)"
         >
-          <div>{{ item.label }}：</div>
-          <div>{{ item.value }}</div>
+          <div class="item-title">{{ item.label }}：</div>
+          <div class="item-content">{{ item.value }}</div>
           <el-icon class="copy-btn" :data-clipboard-text="item.value">
             <component :is="'CopyDocument'"></component>
           </el-icon>
@@ -67,16 +67,118 @@ const regexpList = [
         value: '/^(0|86|17951)?(1[2-9])[0-9]{9}$/'
       },
       {
-        label: '只能输入n个数字',
+        label: '固定电话 XXX-XXXXXXX XXXX-XXXXXXXX',
+        value: '/(\\(\\d{3,4}\\)|\\d{3,4}-|\\s)?\\d{8}/'
+      },
+      {
+        label: '行首行尾空格',
+        value: '/(^\\s*)|(\\s*$)/'
+      },
+      {
+        label: '所有空格',
+        value: '/\\s+/g'
+      },
+      {
+        label: '身份证号',
+        value: '/^(^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$)|(^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])((\\d{4})|\\d{3}[Xx])$)$/'
+      },
+      {
+        label: 'Email地址',
+        value: '/^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$/'
+      },
+      {
+        label: '域名',
+        value: '/^((http:\\/\\/)|(https:\\/\\/))?([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}(\\/)/'
+      },
+      {
+        label: 'IP',
+        value: '/((?:(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.){3}(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d))/'
+      },
+      {
+        label: '帐号 字母开头、5-16位、字母数字下划线',
+        value: '/^[a-zA-Z][a-zA-Z0-9_]{4,15}$/'
+      },
+      {
+        label: '密码 8位以上、字母、数字及特殊符号',
+        value: '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!.,@$%^&*-]).{8,}$/'
+      },
+      {
+        label: 'HTML标签',
+        value: '/<(\\S*?)[`^>]*>.*?</>|<.*? />/'
+      },
+      {
+        label: '16进制颜色',
+        value: '/^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3}|[a-fA-F0-9]{8}|[a-fA-F0-9]{4})$/'
+      },
+    ]
+  },
+  {
+    name: 'number',
+    title: '数字相关',
+    children: [
+      {
+        label: '数字',
+        value: '/^[0-9]*$/'
+      },
+      {
+        label: 'n位数字',
         value: '/^\\d{n}$/'
       },
       {
-        label: '只能输入m到n个数字',
+        label: 'm到n位的数字',
         value: '/^\\d{m,n}$/'
       },
       {
-        label: '匹配行首行尾空白',
-        value: '/(^\\s*)|(\\s*$)/'
+        label: '至少n位以上的数字',
+        value: '/^\\d{n,}$/'
+      },
+      {
+        label: '带1-2位小数的正数或负数',
+        value: '/^(-)?\\d+(.\\d{1,2})?$/'
+      },
+      {
+        label: '正数、负数和小数',
+        value: '/^(-|+)?\\d+(.\\d+)?$/'
+      },
+      {
+        label: '有两位小数的正实数',
+        value: '/^[0-9]+(.[0-9]{2})?$/'
+      },
+      {
+        label: '非零的正整数',
+        value: '/^[1-9]\\d*$/'
+      },
+      {
+        label: '非零的负整数',
+        value: '/^-[1-9]\\d*$/'
+      },
+      {
+        label: '非正整数',
+        value: '/^-[1-9]\\d*|0$/'
+      },
+      {
+        label: '非负整数',
+        value: '/^\\d+$/'
+      },
+      {
+        label: '浮点数',
+        value: '/^(-?\\d+)(.\\d+)?$/'
+      },
+      {
+        label: '正浮点数',
+        value: '/^[1-9]\\d*.\\d*|0.\\d*[1-9]\\d*$/'
+      },
+      {
+        label: '负浮点数',
+        value: '/^-([1-9]\\d*.\\d*|0.\\d*[1-9]\\d*)$/'
+      },
+      {
+        label: '非正浮点数',
+        value: '/^((-\\d+(.\\d+)?)|(0+(.0+)?))$/'
+      },
+      {
+        label: '非负浮点数',
+        value: '/^\\d+(.\\d+)?$/'
       }
     ]
   },
@@ -87,14 +189,6 @@ const regexpList = [
       {
         label: '中文字符',
         value: '/[\\u4e00-\\u9fa5]/gm'
-      },
-      {
-        label: '只能输入数字',
-        value: '/^\\d+$/'
-      },
-      {
-        label: '只能输入至少n个以上的数字',
-        value: '/^\\d{n,}$/'
       },
       {
         label: '只能由英文字母组成',
@@ -270,7 +364,7 @@ onMounted(() => {
   .regexp-wrapper {
     position: relative;
     top: -13px;
-    overflow: hidden;
+    overflow-y: auto;
     border: none;
     margin-left: 20px;
     :deep(.el-collapse-item__header) {
@@ -285,13 +379,20 @@ onMounted(() => {
       padding-bottom: 0;
     }
     .regexp-item {
-      height: 30px;
+      min-height: 30px;
       cursor: pointer;
       line-height: 1;
       &:hover {
         .copy-btn {
           display: block;
         }
+      }
+      .item-title {
+        white-space: nowrap;
+      }
+      .item-content {
+        word-break: break-all;
+        word-wrap: break-word;
       }
       .copy-btn {
         margin-left: 10px;
