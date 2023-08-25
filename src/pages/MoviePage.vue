4518b20787330336df946b2a8c72f064f54b0e4f
Fee <2809750987@qq.com>
2023-08-25 17:16:30 +0800

    save

--- src/pages/MoviePage.vue
+++ src/pages/MoviePage.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="page-wrapper page-movie">
-    <iframe class="movie-iframe" src="https://fee-ing.github.io/Fee-web/#/" frameborder="0"></iframe>
+  <div class="page-wrapper page-movie" id="page-movie">
+    <iframe
+      class="movie-iframe"
+      id="movie-iframe"
+      src="https://fee-ing.github.io/Fee-web/#/"
+      frameborder="0"
+    ></iframe>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { onActivated } from 'vue'
+import { ElLoading } from 'element-plus'
 
-}
+onActivated(() => {
+  const loading = ElLoading.service({
+    lock: true,
+    text: '加载中~',
+    background: 'rgba(255, 255, 255, 0.7)',
+    target: document.getElementById('page-movie')
+  })
+  document.getElementById('movie-iframe').onload = () => {
+    loading.close()
+  }
+})
 </script>
 
 <style lang="less" scoped>
